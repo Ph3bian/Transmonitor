01e6e52489df47c9b7df4c78933c3bf82afcc756
Ph3bian <phebianchukwurah@gmail.com>
2020-05-26 11:20:34 +0100

    include fonts

---
+++ src/pages/Login.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Login = () => <div>Home Page </div>
+
+export default Login
---
+++ src/utils/axios.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import { getDefaultAuth, setLogout } from '../context/AuthContext'
+
+let Axios = axios.create({
+    baseURL: 'https://dummy-endpoint-url.com',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+
+Axios.interceptors.request.use((defaultConfig) => {
+    let config
+    const user = getDefaultAuth()
+
+    if (user) {
+        config = {
+            ...defaultConfig,
+            headers: {
+                ...defaultConfig.headers,
+                Authorization: `Bearer ${user.token}`,
+            },
+        }
+    } else {
+        config = defaultConfig
+    }
+
+    return config
+})
+
+Axios.interceptors.response.use(
+    (response) => {
+        return response
+    },
+    ({ response }) => {
+        if (response && response.status === 401) {
+            return setLogout()
+        }
+        return Promise.reject(response)
+    }
+)
+export default Axios
