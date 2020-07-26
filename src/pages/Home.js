9e93db1dccb56a89b645ace4d9e2b33df2b779e9
Ph3bian <phebianchukwurah@gmail.com>
2020-07-26 22:11:52 +0100

    stats card layout complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import styles from './home.module.scss'
+import { StatsIcon } from 'assets/svg'
 const Home = () => {
   return (
     <div className={styles.Home}>
       <div className={styles.stats}>
         <div className={styles.statsItem}>
           <div></div>
-          <div></div>
+          <div>
+            <StatsIcon />
+          </div>
         </div>
-        <div></div>
-        <div></div> <div></div>
+        <div className={styles.statsItem}>
+          <div></div>
+          <div>
+            <StatsIcon />
+          </div>
+        </div>
+        <div className={styles.statsItem}>
+          <div></div>
+          <div>
+            <StatsIcon />
+          </div>
+        </div>
+        <div className={styles.statsItem}>
+          <div></div>
+          <div>
+            <StatsIcon />
+          </div>
+        </div>
       </div>
       <div className={styles.analytics}></div>
       <div className={styles.payments}></div>
